af9f8252701f77559bc7a6fa3d11a13c12771e51
Martin Mendez <martinpercu@gmail.com>
2023-10-30 18:37:16 +0000

    example use of teleport

---
+++ src/components/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button @click="toggleModal">Modal ON</button>
+        <teleport to='body'>
+            <div v-show="showed" class="modal">
+                <h2>Title Of Modal</h2>
+                <p>Lorem ipsum dolor sit amet  prov id aliqolor, ex nobis iusto.</p>
+                <button @click="toggleModal">Close Modal</button>
+            </div>
+        </teleport>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showed: false
+        }
+    },
+    methods: {
+        toggleModal() {
+            this.showed = !this.showed;
+        }
+    }
+}
+</script>
